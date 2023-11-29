743f14094cc3050a63ae548dab8e98cd3c26519e
Robert Jördens <rj@quartiq.de>
2023-11-29 12:11:39 +0100

    egui 0.24

--- src/bin/psd.rs
+++ src/bin/psd.rs
@@ -223,7 +223,9 @@ fn main() -> Result<()> {
     });
 
     let options = eframe::NativeOptions {
-        initial_window_size: Some((1000.0, 700.0).into()),
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([1000.0, 700.0])
+            .with_drag_and_drop(true),
         ..Default::default()
     };
     eframe::run_native(
